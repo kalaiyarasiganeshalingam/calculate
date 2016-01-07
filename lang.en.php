ae5e30e7b6b5d31165d521853b20cea723681d8f
kalaiyarasi ganeshalingam <ashakalai31@gmail.com>
2016-01-07 17:44:49 +0530

    en

---
+++ lang.en.php
@@ -0,0 +1,12 @@
+<?php
+
+ 
+$lang = array();
+$lang['Select']='Select Language';
+$lang['PAGE_TITLE'] = 'My Calculator';
+$lang['HEADER_TITLE'] = 'Calculator';
+$lang['First_Value'] = 'Enter first value';
+$lang['Second_Value'] = 'Enter Second value';
+$lang['Select_Operation']='Select the operation';
+
+?>
