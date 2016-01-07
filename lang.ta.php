f6441b4f9975465543bc9aa39fcf949232d4b4d9
kalaiyarasi ganeshalingam <ashakalai31@gmail.com>
2016-01-07 17:45:39 +0530

    tamil

---
+++ lang.ta.php
@@ -0,0 +1,12 @@
+<?php
+
+ 
+$lang = array();
+$lang['PAGE_TITLE'] = 'கணிப்பொறி';
+$lang['Select']= 'மொழியை தெரிக';
+$lang['HEADER_TITLE'] = 'கணிப்பொறி';
+$lang['First_Value'] = 'முதலாவது இலக்கத்தை உள்ளிடவும் ';
+$lang['Second_Value'] = 'இரண்டாவது எண்ணை உள்ளிடவும்';
+ $lang['Select_Operation']='';
+
+?>
